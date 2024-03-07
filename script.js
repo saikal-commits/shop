6f468d1b5764c57dfa7fc7212dc0604c34c9d6e9
unknown <sajkal949@gmail.com>
2024-03-07 16:02:51 +0300

    tyyyuu

---
+++ script.js
@@ -0,0 +1,19 @@
+const cards = document.querySelector(".cards");
+
+fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    data.forEach((el, idx) => {
+      cards.innerHTML += `
+        <div>
+    <img src=${el.image} alt="">
+    <span>${el.id}</span>
+    <h3>${el.price}</h3>  
+    <h2>${el.description}</h2>
+    <button class="btn btn-success">add</button>
+
+        </div>
+        `;
+    });
+  });
