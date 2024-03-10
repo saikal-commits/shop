1e10e83abb329569a30e174d79a6676d0f480fac
unknown <sajkal949@gmail.com>
2024-03-10 12:38:21 +0300

    fghjkl;

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+//API
+
 const cards = document.querySelector(".cards");
 
 fetch("https://fakestoreapi.com/products")
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
-    data.forEach((el, idx) => {
-      cards.innerHTML += `
+    data
+      .filter((el) => el.category.includes("electronics"))
+      .sort((a, b) => a.price - b.price)
+      .forEach((el, idx) => {
+        cards.innerHTML += `
         <div>
     <img src=${el.image} alt="">
     <span>${el.id}</span>
-    <h3>${el.price}$</h3>  
-    <h2>${el.description}</h2>
-    <button class="btn btn-success">add</button>
-
+    <h3>${el.title}</h3> 
+    <p>${el.description}</p>
+    <h5>${el.category}</h5>
+    <button class=" delBtn btn btn-success">${el.price}$</button>
         </div>
         `;
-    });
+      });
   });
